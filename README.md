d2a4d19c4c6c0bb1b69679e18b9b752d9f37d3d8
Dale Weiler <weilercdale@gmail.com>
2015-02-02 19:56:36 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# incbin
+
+Include binary files in your C/C++ applications with ease
+
+## Example
+
+    #include "intbin.h"
+
+    INCBIN(Icon, "icon.png");
+
+    // This translation unit now has two symbols
+    // gIconData (unsigned char[])
+    // gIconSize (unsigned int)
+
+## Portability
+
+This will work for any architecture, on all compilers that support GCC inline
+assembler (which is almost all compilers with a few notable exceptions.)
+
+## Alignment
+
+The data included by this tool will be aligned on the architectures word boundary
+unless `__SSE__` or `__neon__` is defined, then it's aligned on a 16-byte boundary,
+so that it can effectively be used by SIMD code safely.
+
+## Explanation
+
+`INCBIN` is a macro which uses the inline assembler provided by almost all
+compilers to include binary files. It achieves this by utilizing the `.incbin`
+directive of the inline assembler. It then uses the assembler to calculate the
+size of the included binary and exports two global symbols that can be externally
+referenced in other translation units which contain the data and size of the
+included binary data respectively.
+
+## Miscellaneous
+
+Documentation for the API is provided by the header. For licensing information
+see UNLICENSE.
---
+++ incbin.h
@@ -0,0 +1,94 @@
+/**
+ * @file incbin.h
+ * @author Dale Weiler
+ * @brief Utility for including binary files
+ *
+ * Facilities for including binary files into the current translation unit and
+ * making use from them externally in other translation units.
+ */
+#ifndef INCBIN_HDR
+#define INCBIN_HDR
+#include <limits.h>
+
+#if defined(__SSE__) || defined(__neon__)
+# define INCBIN_ALIGNMENT 16
+#else
+# if ULONG_MAX == 0xffffffffu
+#  define INCBIN_ALIGNMENT 4
+# else
+#  define INCBIN_ALIGNMENT 8
+# endif
+#endif
+
+#define INCBIN_ALIGN __attribute__((aligned(INCBIN_ALIGNMENT)))
+
+#ifdef __cplusplus
+#  define INCBIN_EXTERNAL extern "C"
+#else
+#  define INCBIN_EXTERNAL extern
+#endif
+
+#define INCBIN_STR(X) #X
+#define INCBIN_STRINGIZE(X) INCBIN_STR(X)
+
+/**
+ * @brief Externally reference binary data included in another translation unit.
+ *
+ * Produces two external symbols that reference the binary data included in
+ * another translation unit.
+ *
+ * The symbol names are a concatenation of "g" before *NAME*; with "Data", as well
+ * as "Size" after. An example is provided below.
+ *
+ * @param NAME The name given for the binary data
+ *
+ * @code
+ * INCBIN_EXTERN(Foo);
+ *
+ * // Now you have the following symbols:
+ * // extern unsigned char gFooData[];
+ * // extern unsigned int gFooSize;
+ * @endcode
+ */
+#define INCBIN_EXTERN(NAME) \
+    INCBIN_EXTERNAL INCBIN_ALIGN unsigned char g ## NAME ## Data[]; \
+    INCBIN_EXTERNAL unsigned int g ## NAME ## Size
+
+/**
+ * @brief Include a binary file into the current translation unit.
+ *
+ * Includes a binary file into the current translation unit, producing two symbols
+ * for objects that encode the data and size respectively.
+ *
+ * The symbol names are a concatenation of "g" before *NAME*; with "Data", as well
+ * as "Size" after. An example is provided below.
+ *
+ * @param NAME The name to associate with this binary data (as an identifier.)
+ * @param FILENAME The file to include (as a string literal.)
+ *
+ * @warning This must be used in global scope
+ *
+ * To externally reference the data included by this in another translation unit
+ * please @see INCBIN_EXTERN.
+ */
+#define INCBIN(NAME, FILENAME) \
+    __asm__(".section .rodata\n" \
+            ".global g" #NAME "\n" \
+            ".type g" #NAME ", @object\n" \
+            ".align " INCBIN_STRINGIZE(INCBIN_ALIGNMENT) " \n" \
+            "g" #NAME ":\n" \
+            "  .incbin \"" FILENAME "\"\n" \
+            ".global g" #NAME "Size\n" \
+            ".type g" #NAME "Size, @object\n" \
+            ".align " INCBIN_STRINGIZE(INCBIN_ALIGNMENT) " \n" \
+            "g" #NAME "Size:\n" \
+            "  .int g" #NAME "Size - g" #NAME "\n"); \
+    INCBIN_EXTERN(NAME)
+
+#undef INCBIN_STRINGIZE
+#undef INCBIN_STR
+#undef INCBIN_EXTERNAL
+#undef INCBIN_ALIGN
+#undef INCBIN_ALIGNMENT
+
+#endif
