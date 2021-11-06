a4353e1526fd96d8af2bbda4811161ff363031be
Dale Weiler <weilercdale@gmail.com>
2021-11-06 15:55:40 -0400

    Update tests

--- test/asserts.c
+++ test/asserts.c
@@ -6,6 +6,12 @@ INCBIN(Lorem, "loremipsum.txt");
 INCBIN(Onebyte, "onebyte.txt");
 INCBIN(Sevenbytes, "sevenbytes.txt");
 
+INCBIN(char, CharLorem, "loremipsum.txt");
+INCBIN(char, CharOnebyte, "onebyte.txt");
+INCBIN(char, CharSevenbytes, "sevenbytes.txt");
+
+INCTXT(TLorem, "loremipsum.txt");
+
 int main(int argc, char **argv)
 {
 	assert(gLoremSize==962);
@@ -16,5 +22,18 @@ int main(int argc, char **argv)
 
 	assert(gSevenbytesSize==7);
 	assert(&gSevenbytesData[gSevenbytesSize] == (const unsigned char*) &gSevenbytesEnd);
+
+	assert(gCharLoremSize==962);
+	assert(&gCharLoremData[gCharLoremSize] == (const char*) &gCharLoremEnd);
+
+	assert(gCharOnebyteSize == 1);
+	assert(&gCharOnebyteData[gCharOnebyteSize] == (const char*) &gCharOnebyteEnd);
+
+	assert(gCharSevenbytesSize==7);
+	assert(&gCharSevenbytesData[gCharSevenbytesSize] == (const char*) &gCharSevenbytesEnd);
+
+	assert(gTLoremSize==963);
+	assert(&gTLoremData[gTLoremSize] == (const char*) &gTLoremEnd);
+
 	exit(0);
 }
